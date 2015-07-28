107bcbd00a6251c57b9147ed09d955324acad963
unknown <test@L-MAA-25004240.paypalcorp.com>
2015-07-28 09:42:35 +0530

    adding the range class

---
+++ src/Range.java
@@ -0,0 +1,171 @@
+import java.util.ArrayList;
+
+
+public class Range {
+	
+	private int start;
+	private int end;
+	
+	public Range(int start, int end)
+	{
+		this.start = start;
+		this.end = end;
+	}
+	
+	public String toString()
+	{
+		return "["+start + ":"+end+"]";
+	}
+	
+	public boolean isLesserRangeThan(Range range)
+	{
+		if(this.end < range.start)
+		{
+			return true;
+		}
+		return false;
+	}
+	public boolean isMergeRequired(Range range2)
+	{
+		if(this.end == range2.start - 1 || range2.end == this.start - 1)
+		{
+			return true;
+		}
+		else if(isOverlapping(range2) || isSubrange(range2))
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isSplitRequired(Range range)
+	{
+		if(isTwoWaySplit(range) || isThreeWaySplit(range))
+		{
+			return true;
+		}
+		if(isOverlapping(range))
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	boolean isSubrange(Range range) {
+		if(range.end <= this.end && range.start >= this.start)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isOverlapping(Range range) {
+		if(!isSubrange(range) && !range.isSubrange(this))
+		{
+			if((this.end >= range.start && this.end <= range.end) || (range.end >= this.start && range.end <= this.end))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean isThreeWaySplit(Range range2)
+	{
+		if(this.start < range2.start && this.end > range2.end)
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isTwoWaySplit(Range range2)
+	{
+		if((this.start < range2.start && this.end == range2.end) || (this.start == range2.start && this.end > range2.end))
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	public Range merge(Range range2)
+	{
+		Range range = new Range(Math.min(this.start,range2.start), Math.max(this.end, range2.end));
+		return range;
+	}
+	
+	public ArrayList<Range> split(Range range)
+	{
+		if(isTwoWaySplit(range))
+		{
+			return twoWaySplit(range);
+		}
+		if(isThreeWaySplit(range))
+		{
+			return threeWaySplit(range);
+		}
+		if(isOverlapping(range))
+		{
+			return overlapSplit(range);
+		}
+		return null;
+	}
+	
+	private ArrayList<Range> overlapSplit(Range range) {
+		ArrayList<Range> splitted = new ArrayList<Range>();
+		if(this.start < range.start)
+		{
+			splitted.add(new Range(this.start,range.start - 1));
+			splitted.add(new Range(range.start,range.end));
+		}
+		else
+		{
+			splitted.add(new Range(range.start, range.end));
+			splitted.add(new Range(range.end+1,this.end));
+		}
+		return splitted;
+	}
+
+	public ArrayList<Range> twoWaySplit(Range range)
+	{
+		ArrayList<Range> splitted = new ArrayList<Range>();
+		if(this.end == range.end)
+		{
+			splitted.add(new Range(Math.min(this.start,range.start),Math.max(this.start, range.start) - 1));
+			splitted.add(new Range(Math.max(this.start, range.start), this.end));
+		}
+		else
+		{
+			splitted.add(new Range(this.start,Math.min(this.end, range.end)));
+			splitted.add(new Range(Math.min(this.end, range.end)+1, this.end));
+		}
+		return splitted;
+	}
+	
+	public ArrayList<Range> threeWaySplit(Range range)
+	{
+		ArrayList<Range> splitted = new ArrayList<Range>();
+		splitted.add(new Range(this.start,range.start - 1));
+		splitted.add(new Range(range.start, range.end));
+		splitted.add(new Range(range.end + 1, this.end));
+		return splitted;
+	}
+	
+	public static void main(String args[])
+	{
+		Range range1 = new Range(1, 100);
+		Range range2 = new Range(120, 150);
+		Range range3 = new Range(101, 200);
+		Range range4 = new Range(130, 200);
+		Range range5 = new Range(1,300);
+		System.out.println(range1);
+		System.out.println(range1.isMergeRequired(range2));
+		System.out.println(range1.isMergeRequired(range3));
+		System.out.println(range1.isMergeRequired(range4));
+		System.out.println(range5.isSplitRequired(range1));
+		System.out.println(range5.split(range1));
+		System.out.println(range4.isSplitRequired(range2));
+		System.out.println(range4.split(range2));
+	}
+
+}
---
+++ src/Tracking.java
@@ -0,0 +1,121 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Tracking {
+
+	private String statusCode;
+	private int trackingNumber;
+	private Range range;
+	
+	public Tracking()
+	{
+		
+	}
+	public String toString()
+	{
+		return range + " " + statusCode + " " + trackingNumber;
+	}
+	
+	public Tracking(String statusCode, int trackingNumber, Range range){
+		this.statusCode = statusCode;
+		this.trackingNumber = trackingNumber;
+		this.range = range;
+	}
+	
+	public static List<Tracking> readInput(String filename) throws IOException {
+		List<Tracking> trackingList = new ArrayList<Tracking>();
+		Scanner sc = new Scanner(new FileReader(filename));
+		String currentLine;
+		while(sc.hasNext()){
+			currentLine = sc.nextLine();
+			String[] splitLine = currentLine.split(" ");
+			Range range = new Range(Integer.parseInt(splitLine[0]), Integer.parseInt(splitLine[1]));
+			Tracking tracking = new Tracking(splitLine[2], Integer.parseInt(splitLine[3]), range);
+			trackingList.add(tracking);	
+		}
+		return trackingList;
+	}
+	
+	public boolean isStatusCodeAndTrackingNumSame(Tracking trackingObj1, Tracking trackingObj2)
+	{
+		if(trackingObj1.statusCode.equals(trackingObj2.statusCode) && trackingObj1.trackingNumber  == trackingObj2.trackingNumber)
+			return true;
+		return false;
+	}
+	public List<Tracking> generateMinimalTrackingList(List<Tracking> trackingInputList) {
+		//Range range;
+		List <Tracking> minimalTrackingList = new ArrayList<Tracking>();
+		//List<Range> rangeList = new ArrayList<Range>();
+		Tracking trackObj;
+		minimalTrackingList.add(trackingInputList.get(0));
+		for(int i = 1; i < trackingInputList.size(); i++){
+			System.out.println(i);
+			for(int j = 0; j < minimalTrackingList.size(); j++)
+			{
+				System.out.println(minimalTrackingList);
+				//System.out.println(trackingInputList);
+				System.out.println(i + ":" + j);
+				if(trackingInputList.get(i).range.isLesserRangeThan(minimalTrackingList.get(j).range))
+				{
+					System.out.println("enters2");
+					minimalTrackingList.add(j, trackingInputList.get(i));
+					break;
+				}
+				else if(isStatusCodeAndTrackingNumSame(trackingInputList.get(i), minimalTrackingList.get(j)))
+				{
+					System.out.println("enters1");
+					if(minimalTrackingList.get(j).range.isMergeRequired(trackingInputList.get(i).range))
+					{
+						System.out.println("enters");
+						trackObj = new Tracking();
+						trackObj.range = trackingInputList.get(i-1).range.merge(trackingInputList.get(i).range);
+						trackObj.statusCode = trackingInputList.get(i).statusCode;
+						trackObj.trackingNumber =  trackingInputList.get(i).trackingNumber;
+						minimalTrackingList.add(j, trackObj);
+						break;
+					}
+				}
+				else
+				{
+					if(minimalTrackingList.get(j).range.isSplitRequired(trackingInputList.get(i).range))
+					{
+						ArrayList<Range> splitRanges = minimalTrackingList.get(j).range.split(trackingInputList.get(i).range);
+						ArrayList<Tracking> trackingAssignedList = assignStatusAndTrackNumberToRange(splitRanges, trackingInputList.get(i), minimalTrackingList.get(j));
+						minimalTrackingList.addAll(j, trackingAssignedList);
+						break;
+					}
+				}
+			}
+		}
+		return minimalTrackingList;
+		
+	}
+	
+	private static ArrayList<Tracking> assignStatusAndTrackNumberToRange(ArrayList<Range> splitRanges, Tracking trackingObj1, Tracking trackingObj2) {
+		ArrayList<Tracking> trackingAssignedList = new ArrayList<Tracking>();
+		Tracking tracking = new Tracking();
+		
+		for(int i = 0; i < splitRanges.size(); i++){
+			if(trackingObj1.range.isSubrange(splitRanges.get(i)))
+			{
+				tracking = new Tracking(trackingObj1.statusCode, trackingObj1.trackingNumber, splitRanges.get(i));
+			}
+			if(trackingObj2.range.isSubrange(splitRanges.get(i)))
+			{
+				tracking = new Tracking(trackingObj2.statusCode, trackingObj2.trackingNumber, splitRanges.get(i));
+			}
+			trackingAssignedList.add(tracking);
+		}
+ 		return trackingAssignedList;
+	}
+	
+	public static void main(String [] args) throws IOException {
+		List<Tracking> trackingInputList = readInput("C:\\Users\\test\\Downloads\\TrackingRecord.txt");
+		//System.out.println(trackingInputList);
+		Tracking tracking = new Tracking();
+		List<Tracking> minimalTrackingList = tracking.generateMinimalTrackingList(trackingInputList);
+		System.out.println("Ans:"+minimalTrackingList);
+	}
+	
+}
